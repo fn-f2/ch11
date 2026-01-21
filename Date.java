f5fc113cc0968c7a137e574840ad222d21bee32a
carter <89955530+fn-f2@users.noreply.github.com>
2026-01-20 17:35:32 -0800

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,17 @@
+public class Date
+{
+    private int year = 8082;
+    private int month = 1;
+    private int day = 9;
+    
+    public Date()
+    {
+    }
+    
+    public Date(int year, int month, int day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
---
+++ DateDriver.java
@@ -0,0 +1,7 @@
+public class DateDriver
+{
+    public static void main(String[] args)
+    {
+        Date birthday = new Date(2000, 1, 1);
+    }
+}
